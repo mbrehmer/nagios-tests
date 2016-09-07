183ad06d374596c2f4c823dfb967eacb327102bb
Martin Brehmer <git@mbrehmer.de>
2016-09-07 15:28:18 +0200

    [ADD] parameter processing
    This commit adds the parameter processing to the script.
    
    The following parameters are added:
    
      -l, --listen <IP_ADDRESS>      Specify the listening address
      -p, --port <PORT_NUMBER>       The port number to test
      -P, --protocoll <tcp|udp|any>  The upper-layer protocol to test for
      -v, --verbose                  verbose output
      -V, --version                  print version information

--- check_port_listener.sh
+++ check_port_listener.sh
@@ -26,6 +26,7 @@
 VERSION=0.1
 
 STATE_OK=0
+STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
 STATE_DEPENDENT=4
@@ -38,8 +39,69 @@ print_version()
 $PROG_NAME $VERSION
 
 Copyright (C) 2016 Martin Brehmer <software@mbrehmer.de>
+
 This is free software.  You may redistribute copies of it under the terms of
 the GNU General Public License <http://www.gnu.org/licenses/gpl.html>.
 There is NO WARRANTY, to the extent permitted by law.
 EOF
 }
+
+SHORT_OPTS="p:P:l:vV"
+LONG_OPTS="port,protocoll,listen,version,verbose"
+
+# parameter processing
+$(getopt -T >/dev/null 2>&1)
+if [[ $? -eq 4 ]]; then
+	OPTS=$(getopt -o $SHORT_OPTS --long $LONG_OPTS -n "$PROG_NAME" -- "$@")
+else
+	case $1 in
+		--version)
+			print_version
+			exit 0
+		;;
+	esac
+	OPTS=$(getopt $SHORT_OPTS "$@")
+fi
+
+eval set -- "$OPTS"
+
+declare -i PORT
+
+# default values
+PORT=0
+ADDRESS="0.0.0.0"
+PROTOCOL="tcp"
+VERBOSE=false
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-V|--version)
+			print_version
+			exit 0
+			;;
+		-l|--listen)
+			ADDRESS=$2
+			shift 2
+			;;
+		-p|--port)
+			PORT=$2
+			shift 2
+			;;
+		-P|--protocol)
+			PROTOCOL=$2
+			shift 2
+			;;
+		-v|--verbose)
+			VERBOSE=true
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Internal Error: parameter processing error: $1" >&2
+			exit 1
+			;;
+	esac
+done
