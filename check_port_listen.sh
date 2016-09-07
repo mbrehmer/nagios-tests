dc35c85d4f22450f00ddef456c86486e87168467
Martin Brehmer <git@mbrehmer.de>
2016-09-07 14:23:13 +0200

    [ADD] Copyright and License information

--- check_port_listen.sh
+++ check_port_listen.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+################################################################################
+#                                                                              #
+# A Nagios Plugin that checks if there is any program listening on a specified #
+# TCP/UDP port.                                                                #
+#                                                                              #
+# Copyright (C) 2016 by Martin Brehmer <software@mbrehmer.de>                  #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation; either version 2, or (at your option)          #
+# any later version.                                                           #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software Foundation,      #
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.           #
+#                                                                              #
+################################################################################
